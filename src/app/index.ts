26f3151772a283ec0e03d8dae7754d2f8ac1a70f
luizaoformigapqd <luizlima@garantti.com.br>
2022-03-27 09:29:15 -0300

    feat: 🎸 05 - Configurando database

--- src/app/index.ts
+++ src/app/index.ts
@@ -5,15 +5,18 @@ import { createConnection } from "typeorm";
 import express from "express";
 import cors from "cors";
 import { router } from "./routes/routes";
+import path, { join } from "path";
+import { User } from "./entity/User";
 
 const app = express();
 const port = 5000;
 
 createConnection({
   type: "sqlite",
-  database: "gowftech.db",
-  entities: [__dirname + "/entity.{js,ts}"],
+  database: "typeorm.db",
+  entities: [User],
   synchronize: true,
+  logging: false
 });
 
 app.use(cors());
